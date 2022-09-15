4c4191550f04dc8597832ca35f54fe32fa12f937
Vladimir <vladimir.kovalenko@combridge-itc.de>
2022-09-15 10:12:22 +0300

    init commit

---
+++ src/main/java/ru/vsu/cs/v72/Task/TaskPlayersApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.v72.Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/cs/v72/Task/TaskPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.v72.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
